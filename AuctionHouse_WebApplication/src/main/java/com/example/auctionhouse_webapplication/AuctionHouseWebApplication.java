5f52112e301cf3150389367616dc57813feb54ac
Hibah Hussain <110937813+HKH2014@users.noreply.github.com>
2023-10-30 09:44:08 +0000

    code for project

---
+++ AuctionHouse_WebApplication/src/main/java/com/example/auctionhouse_webapplication/AuctionHouseWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.auctionhouse_webapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionHouseWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionHouseWebApplication.class, args);
+    }
+
+}
---
+++ AuctionHouse_WebApplication/src/test/java/com/example/auctionhouse_webapplication/AuctionHouseWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auctionhouse_webapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionHouseWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
